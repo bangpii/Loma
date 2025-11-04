70230a90f2b75aae0cec093389b0fb2ace5c7075
baihaqie <baihaqiearrafi@students.polmed.ac.id>
2025-11-04 17:54:37 +0700

    HARINI

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import {
+    defineConfig
+} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        chunkSizeWarningLimit: 1500, // ubah batas peringatan jadi 1.5 MB
+    },
 })
